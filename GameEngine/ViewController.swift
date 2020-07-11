b64ff36ca0d8b3aeeb7f28f8f5cf6d507755bd42
VictorPuga <victorpugaruiz@live.com.mx>
2020-07-10 20:52:37 -0600

    Initial Commit

---
+++ GameEngine/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  GameEngine
+//
+//  Created by Victor Manuel Puga Ruiz on 10/07/20.
+//  Copyright © 2020 Ampersand Labs. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
