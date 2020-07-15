9fb35a5bb4703a1df4aa50fe2d0912a985b1bcaa
VictorPuga <victorpugaruiz@live.com.mx>
2020-07-15 12:43:30 -0600

    Colored triangle

--- GameEngine/GameView.swift
+++ GameEngine/GameView.swift
@@ -10,30 +10,32 @@ import MetalKit
 
 class GameView: MTKView {
     
+    struct Vertex {
+        var position: float3
+        var color: float4
+    }
+    
     var commandQueue: MTLCommandQueue!
     var renderPipelineState: MTLRenderPipelineState!
-    let vertices: [float3] = [
-        float3( 0,  1, 0),
-        float3(-1, -1, 0),
-        float3( 1, -1, 0)
-    ]
+    var vertices: [Vertex]!
     var vertexBuffer: MTLBuffer!
     
     required init(coder: NSCoder) {
         super.init(coder: coder)
         self.device = MTLCreateSystemDefaultDevice()
-        self.clearColor = MTLClearColor(red: 1, green: 0, blue: 0, alpha: 1)
+        self.clearColor = MTLClearColor(red: 1, green: 0.647, blue: 0, alpha: 1)
         self.colorPixelFormat = .bgra8Unorm
         self.commandQueue = device?.makeCommandQueue()
         
         createRenderPipelineState()
+        createVertices()
         createBuffers()
     }
     
     func createBuffers()  {
         vertexBuffer = device?.makeBuffer(
             bytes: vertices,
-            length: MemoryLayout<float3>.stride * vertices.count,
+            length: MemoryLayout<Vertex>.stride * vertices.count,
             options: []
         )
     }
@@ -53,6 +55,14 @@ class GameView: MTKView {
         }catch let error as NSError {
             print(error)
         }
+    }
+    
+    func createVertices() {
+        vertices =  [
+            Vertex(position: float3( 0,  1, 0), color: float4(1,0,0,1)),
+            Vertex(position: float3(-1, -1, 0), color: float4(0,1,0,1)),
+            Vertex(position: float3( 1, -1, 0), color: float4(0,0,1,0))
+        ]
     }
     
     override func draw(_ rect: NSRect) {
