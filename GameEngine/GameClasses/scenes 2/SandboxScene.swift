ca5874174d38cc916c33146d5fa64e2f3d89cec9
VictorPuga <victorpugaruiz@live.com.mx>
2020-08-06 08:15:58 -0600

    Fix

---
+++ GameEngine/GameClasses/scenes 2/SandboxScene.swift
@@ -0,0 +1,23 @@
+//
+//  SandboxScene.swift
+//  GameEngine
+//
+//  Created by Victor Manuel Puga Ruiz on 19/07/20.
+//  Copyright © 2020 Ampersand Labs. All rights reserved.
+//
+
+import MetalKit
+
+class SandboxScene: Scene{
+    
+    var debugCamera = DebugCamera()
+    var quad = Quad()
+    override func buildScene() {
+        addCamera(debugCamera)
+        
+        debugCamera.setPositionZ(5)
+
+        addChild(quad)
+    }
+    
+}
